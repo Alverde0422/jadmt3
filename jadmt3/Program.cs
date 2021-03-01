26bd0cf249c61c35eb2553bf464fcc63c3bf0d14
Mohamed Khader Abdelaziz Mahafel <moha9570@zbc.dk>
2021-03-01 10:37:48 +0100

    Add project files.

---
+++ jadmt3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_mind_tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            Dictionary<string, int> people = new Dictionary<string, int>
+            {
+                // Opgave 2
+                { "mark", 18 },
+                { "skayrunner", 88 }
+            };
+            people["jammy"] = 18;
+
+            // ??
+            Console.WriteLine(people.First() + "\n");
+
+            people.Remove("skayrunner");
+
+
+            foreach (var p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
